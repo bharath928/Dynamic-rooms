ab30b30be90ce59071182bfd688fa29b67273e4f
jagadeesh <dasarijagadeesh442@gmail.com>
2025-01-24 21:14:18 +0530

    new content

--- backend/controllers/floorsController.js
+++
@@ -1,9 +0,0 @@
-const block = require("../models/blockmodel")
-
-// const createFloor = ()=>{
-//     const {id} = req.params;
-
-//     const container = block.findById(id);
-//     container.floors.push()
-// }
-
